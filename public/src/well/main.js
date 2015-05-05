f0cfe0da465449fda4ccfac406564280d849f976
ramanathanMuthuraman <ramanathan.gksvv@gmail.com>
2015-05-05 23:03:59 +0530

    added input

--- public/src/well/main.js
+++ public/src/well/main.js
@@ -2,20 +2,14 @@
 var React = require('react');
 
 var Well = require('react-bootstrap').Well;
+var Input =  require('../textfield/main');
 var Col = require('react-bootstrap').Col;
 var Button = require('react-bootstrap').Button;
  module.exports = React.createClass({
     render:function(){
       return     <Well className = "container-fluid caption-well">
       				<Col xs={12} md={12} > 
-      					<div className="jumbotron"> <h1>React Boilerplate</h1>
-      					<p>
-      						This is a react boilerpate exhibiting isomorphic javascript feature. 
-      					</p>
-      				    <p>
-                            <a className="btn btn-primary custom_link" target="blank" href="https://github.com/ramanathanMuthuraman/react-isomorphic-boilerplate">Learn more</a>
-      					</p>
-      					</div>
+      					<Input type='text'  />
       				</Col>
       			 </Well>
     }
